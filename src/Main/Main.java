e7640db8e13db388361a8aa45eb3609b3fb4b6cb
Nat-200606 <nataliamaciel20006@gmail.com>
2026-04-07 08:43:17 -0300

    First attempt to learn how a game in java works

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("My first game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
